3bb17a40c809bcc18d578cc1e0a73528c97fc5fa
McKenzie Addison <146353691+heinskeys@users.noreply.github.com>
2024-10-14 23:13:41 -0400

    cleaned up comments/added some responsiveness

--- alphabuild/alphabuild.js
+++ alphabuild/alphabuild.js
@@ -1,23 +1,18 @@
-// Get the modal
+// Modal base code from W3Schools
 var addedmodal = document.getElementById("addedModal");
 
-// Get the button that opens the modal
-var addedButton = document.getElementById("addtocartButton"); // Changed the ID to match HTML
+var addedButton = document.getElementById("addtocartButton"); 
 
-// Get the <span> element that closes the modal
 var closeButton = document.getElementsByClassName("addButtonClose")[0];
 
-// When the user clicks on the button, open the modal
 addedButton.onclick = function() {
-    addedmodal.style.display = "block";  // Fixed the missing dot before 'style'
+    addedmodal.style.display = "block"; 
 }
 
-// When the user clicks on <span> (x), close the modal
 closeButton.onclick = function() {
-    addedmodal.style.display = "none"; // Close the modal, not just the closeButton
+    addedmodal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == addedmodal) {
         addedmodal.style.display = "none";
